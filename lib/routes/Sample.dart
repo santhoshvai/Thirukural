0e7a5d13d820b3c60b6fde66bc783b2e90532464
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2018-02-10 23:03:16 +0100

    update gitignore and animated list sample

--- lib/routes/Sample.dart
+++ lib/routes/Sample.dart
@@ -42,7 +42,7 @@ class _AnimatedListSampleState extends State<AnimatedListSample> {
   }
 
   // Used to build an item after it has been removed from the list. This method is
-  // needed because a removed item remains  visible until its animation has
+  // needed because a removed item remains visible until its animation has
   // completed (even though it's gone as far this ListModel is concerned).
   // The widget will be used by the [AnimatedListState.removeItem] method's
   // [AnimatedListRemovedItemBuilder] parameter.
@@ -101,6 +101,50 @@ class _AnimatedListSampleState extends State<AnimatedListSample> {
       ),
     );
   }
+}
+
+/// Keeps a Dart List in sync with an AnimatedList.
+///
+/// The [insert] and [removeAt] methods apply to both the internal list and the
+/// animated list that belongs to [listKey].
+///
+/// This class only exposes as much of the Dart List API as is needed by the
+/// sample app. More list methods are easily added, however methods that mutate the
+/// list must make the same changes to the animated list in terms of
+/// [AnimatedListState.insertItem] and [AnimatedList.removeItem].
+class ListModel<E> {
+  ListModel({
+    @required this.listKey,
+    @required this.removedItemBuilder,
+    Iterable<E> initialItems,
+  }) : assert(listKey != null),
+        assert(removedItemBuilder != null),
+        _items = new List<E>.from(initialItems ?? <E>[]);
+
+  final GlobalKey<AnimatedListState> listKey;
+  final dynamic removedItemBuilder;
+  final List<E> _items;
+
+  AnimatedListState get _animatedList => listKey.currentState;
+
+  void insert(int index, E item) {
+    _items.insert(index, item);
+    _animatedList.insertItem(index);
+  }
+
+  E removeAt(int index) {
+    final E removedItem = _items.removeAt(index);
+    if (removedItem != null) {
+      _animatedList.removeItem(index, (BuildContext context, Animation<double> animation) {
+        return removedItemBuilder(removedItem, context, animation);
+      });
+    }
+    return removedItem;
+  }
+
+  int get length => _items.length;
+  E operator [](int index) => _items[index];
+  int indexOf(E item) => _items.indexOf(item);
 }
 
 /// Displays its integer item as 'item N' on a Card whose color is based on
